6b98ac0a1134377be1b05dde054d6f7d57dc1773
Jainil2 <chauhanjainil305@gmail.com>
2025-02-16 21:34:44 +0530

    complete testing of like and comment

--- user-dashboard-server/src/index.js
+++ user-dashboard-server/src/index.js
@@ -22,5 +22,5 @@ app.use("/", reactionRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
